b6b5a85af755c02a2a2767c3491eaf1a9e5ab87d
Minato512 <akagishotaro44@gmail.com>
2021-11-16 18:07:15 +0900

    fix

--- src/views/Withdraw.js
+++ src/views/Withdraw.js
@@ -37,6 +37,7 @@ const Withdraw = () => {
   };
   const getBalance = async () => {
     if (web3) {
+      const address = await web3.eth.getAccounts();
       const contract = new web3.eth.Contract(
         travelABI,
         "0x60156A5D89b3761DF7f65d5A84A4F117B9597957"
